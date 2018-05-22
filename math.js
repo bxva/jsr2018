c88a3fa5d502854dc6b4c604e06090f6db86ce9f
BernardoVallarino <bxva@utexas.edu>
2018-05-22 16:22:38 -0500

    HW 2
    Homework 2

---
+++ math.js
@@ -0,0 +1,21 @@
+git add .
+git commit -m "Homework 2"
+git push
+
+
+var a = 5;
+var b = 6;
+var c = 7;
+
+var p = (a+b+c)/2;
+
+var area = sqrt(p * (p-a) * (p-b) * (p-c));
+
+console.log(area);
+prompt (area);
+
+prompt (a);
+prompt (b);
+prompt (c);
+
+alert(area);
